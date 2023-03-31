b0761127de185188719ec5e49c35690ce407bf8a
PotatarZ <holgersonpeter@gmail.com>
2023-03-30 22:10:44 -0400

    Gauge added

--- static/js/app.js
+++ static/js/app.js
@@ -14,6 +14,15 @@ dataPromise.then(data => {
 const metaDataBox = d3.select('#sample-metadata');
 const dropdownMenu = d3.select('#selDataset');
 
+// Function for getting arrowTip location for gauge
+function getAngle(wfreq) {
+    var theta = 180 - 180 / 9 * wfreq
+    var r = 0.35
+    var x_head = r * Math.cos(Math.PI/180*theta)
+    var y_head = r * Math.sin(Math.PI/180*theta)
+    return [x_head, y_head]
+}
+
 // Initialize charts and dropdown menu
 function init() {
 
@@ -29,9 +38,9 @@ function init() {
 
     // Horizontal bar chart
     barTrace = {
-        x: samplesData.sample_values.slice(0, 11),
-        y: samplesData.otu_ids.slice(0, 11).map(id => `OTU-${id}`),
-        text: samplesData.otu_labels.slice(0, 11),
+        x: samplesData.sample_values.slice(0, 10),
+        y: samplesData.otu_ids.slice(0, 10).map(id => `OTU-${id}`),
+        text: samplesData.otu_labels.slice(0, 10),
         type: 'bar',
         orientation: 'h',
         transforms: [{
@@ -40,7 +49,10 @@ function init() {
             order: 'ascending'
         }]
     };
-    Plotly.newPlot('bar', [barTrace]);
+    barLayout = {
+        title: 'Top Ten Bacteria'
+    };
+    Plotly.newPlot('bar', [barTrace], barLayout);
 
     // Bubble chart
     bubbleTrace = {
@@ -50,16 +62,71 @@ function init() {
         mode: 'markers',
         marker: {
             size: samplesData.sample_values,
-            color: samplesData.otu_ids
+            color: samplesData.otu_ids,
+            colorscale: 'Earth'
         }
     };
-    Plotly.newPlot('bubble', [bubbleTrace]);
+    bubbleLayout = {
+        xaxis: { title: {text: 'OTU id'}}
+    }
+    Plotly.newPlot('bubble', [bubbleTrace], bubbleLayout);
 
     // Demographics
     for (const [key, value] of Object.entries(metadata)) {
         let p = metaDataBox.append('p');
         p.text(`${key}: ${value}`)
     }
+
+    // Washings guage
+    function getSteps() {
+        let steps = [];
+        for (j=0; j<9; j++) {
+            let range = [j, j+1];
+            let color = `rgb(${230 - j*30}, ${j*30}, 0)`;
+            let step = {range:range, color:color};
+            steps.push(step);
+        }
+        return steps;
+    };
+
+    let washTrace = {
+        domain: {x: [0, 1], y: [0, 1]},
+        value: metadata.wfreq,
+        title: {text: 'Belly Button Scrubs per Week'},
+        type: 'indicator',
+        mode: 'gauge',
+        gauge: {
+            axis: {
+                range: [null, 9],
+                tickmode: 'linear',
+                ticks: 'inside',
+                ticklabelstep: 1
+            },
+            steps: getSteps(),
+            bar: {thickness:0},
+        },
+    }
+
+    var arrowTip = getAngle(metadata.wfreq)
+
+    let washLayout = {
+        xaxis: {range: [0, 1], showgrid: false, 'zeroline': false, 'visible': false},
+        yaxis: {range: [0, 1], showgrid: false, 'zeroline': false, 'visible': false},
+        showlegend: false,
+        annotations: [{
+            ax: 0.5,
+            ay: 0.25,
+            axref: 'x',
+            ayref: 'y',
+            x: 0.5+arrowTip[0],
+            y: 0.25+arrowTip[1],
+            xref: 'x',
+            yref: 'y',
+            showarrow: true,
+            arrowhead: 9,
+        }]
+    }
+    Plotly.newPlot('gauge', [washTrace], washLayout)
 }
 
 // Update function based on dropdown selection
@@ -70,9 +137,9 @@ function optionChanged(id) {
     const metadata = otuData.metadata.filter(item => item.id === parseInt(id))[0];
 
     // Update horizontal bar chart
-    let barX = sampleData.sample_values.slice(0, 11);
-    let barY = sampleData.otu_ids.slice(0, 11).map(id => `OTU-${id}`);
-    let barText = sampleData.otu_labels.slice(0, 11);
+    let barX = sampleData.sample_values.slice(0, 10);
+    let barY = sampleData.otu_ids.slice(0, 10).map(id => `OTU-${id}`);
+    let barText = sampleData.otu_labels.slice(0, 10);
     Plotly.update('bar', {x:[barX], y:[barY], text:[barText]});
 
     // Update bubble chart
@@ -86,8 +153,21 @@ function optionChanged(id) {
     metaDataBox.html(null);
     for (const [key, value] of Object.entries(metadata)) {
         let p = metaDataBox.append('p');
-        p.text(`${key}: ${value}`)
+        p.text(`${key}: ${value}`);
     }
+
+    // Update gauge
+    let arrowTip = getAngle(metadata.wfreq)
+    Plotly.update('gauge', {value:[metadata.wfreq]}, {annotations:[{
+        ax: 0.5,
+        ay: 0.25,
+        axref: 'x',
+        ayref: 'y',
+        x: 0.5+arrowTip[0],
+        y: 0.25+arrowTip[1],
+        xref: 'x',
+        yref: 'y',
+    }]})
 }
 
 // Initialize after dataPromise if fulfilled
